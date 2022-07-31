3ac5f33f47a8e5d48e02672b0ffa1476578b9550
Brevenn <Brevenjh@gmail.com>
2022-07-31 01:25:43 -0500

    finished search bar displaying function

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,5 +10,12 @@ initWeather();
 function renderCities(){
     $("#cityList").empty();
     $("#cityInput").val("");
-    
+
+    for(i=0; i<citylist.length; i++){
+        var a = $("<a>");
+        a.addClass("list-group-item list-group-item-action list-group-item-primary city");
+        a.attr("data-name", citylist[i]);
+        a.text(cityList[i]);
+        $("cityList").prepend(a);
+    }
 }
