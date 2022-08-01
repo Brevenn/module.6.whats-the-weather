64dcb70abe917cdb21718e07af442cad8b466967
Brevenn <Brevenjh@gmail.com>
2022-08-01 18:29:31 -0500

    added functions to save information entered by user into local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,36 @@ function renderCities(){
         $("cityList").prepend(a);
     }
 }
+
+// Create a function that pulls the city city list.
+function initCityList() {
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
+
+    if (storedCities !== null) {
+        cityList = storedCities;
+    }
+
+    renderCities();
+}
+
+// Create a function to pull the current city entered by the user into localStorage
+function initWeather() {
+        var storedWeather = JSON.parse(localStorage.getItem("currentCity"));
+
+        if (storedWeather !== null) {
+            cityname = storedWeather;
+
+            displayWeather();
+            displayFiveDayForecast();
+        }
+}
+
+// Create a function to save the city list into the localStorage
+function storeCityArray() {
+    localStorage.setItem("cities", JSON.stringify(cityList));
+}
+
+// Create a function to save the current city enetered and displaying into localStorage
+function storeCurrentCity() {
+    localStorage.setItem("currentCity", JSON.stringify(cityname));
+}
