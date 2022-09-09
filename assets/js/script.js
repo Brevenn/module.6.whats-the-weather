94c28aa7a19bb99f683f9ebe329cc17b800f51b6
Brevenn <Brevenjh@gmail.com>
2022-09-08 21:02:41 -0500

    added click functions and a submit function using jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,48 @@ var currentTemp = $('#current-temp');
 var currentHumidity = $('#current-humidity');
 var currentWindSpeed = $('#current-wind-speed');
 var UVindex = $('#uvindex');
+var weatherContent = $('#weather-content');
+
+// Create access to the OpenWeatheAPI with a key
+var APIkey = "";
+
+// Create a data array for cities
+var cityList = [];
+
+// display current date
+var currentDate = moment().format('L');
+$("#current-date").text("(" + currentDate + ")");
+
+// display search history
+initializeHistory();
+showClear();
+
+
+$(document).on("submit", function(){
+    event.preventDefault();
+
+    var searchValue = searchCityInput.val().trim();
+
+    currentConditionsRequest(searchValue);
+    searchHistory(searchValue);
+    searchCityInput.val("");
+});
+
+searchCityButton.on("click", function(event) {
+    event.preventDefault();
+
+    var searchValue = searchCityInput.val().trim();
+
+    currentConditionsRequest(searchValue);
+    searchHistory(searchValue);
+    searchCityInput.val("");
+});
+
+clearHistoryButton.on("click", function() {
+
+    cityList = [];
+
+    listArray();
+
+    $(this).addClass("hide");
+});
